72ff11ae1940238570e15b206c91cdaf19648da4
supriseeeeef <romankorniiets@gmail.com>
2026-05-30 11:54:41 +0300

    done

---
+++ script.js
@@ -0,0 +1,35 @@
+"use strict";
+function getPromise() {
+    return new Promise((resolve) => {
+        resolve(["Text", 50]);
+    });
+}
+getPromise().then((data) => {
+    console.log(data);
+});
+function compare(top, bottom) {
+    return {
+        name: top.name,
+        color: top.color,
+        position: bottom.position,
+        weight: bottom.weight,
+    };
+}
+function merge(objA, objB) {
+    return Object.assign(objA, objB);
+}
+class Component {
+    props;
+    constructor(props) {
+        this.props = props;
+    }
+}
+class Page extends Component {
+    pageInfo() {
+        console.log(this.props.title);
+    }
+}
+const page = new Page({
+    title: "Home Page",
+});
+page.pageInfo();
